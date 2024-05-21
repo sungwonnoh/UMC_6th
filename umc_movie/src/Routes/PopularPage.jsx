5d75737668ee93e244d70f4185091d31679344a0
sungwonnoh <jessica20730@naver.com>
2024-05-21 21:50:57 +0900

    pagination 하는중

--- umc_movie/src/Routes/PopularPage.jsx
+++ umc_movie/src/Routes/PopularPage.jsx
@@ -10,23 +10,40 @@ const Container = styled.div`
   justify-content: center;
   width: 100vw;
 `;
+const Paginate = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 5px;
+`;
 export default function Popular() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
   const navigate = useNavigate();
+  const pageList = [];
+  const totalPages = Math.ceil();
+  for (let i = 1; i <= totalPages; i++) {
+    pageList.push(i);
+  }
+  //
   /*setTimeout(() => {
     setLoading(false);
   }, 5000);
 */
-  const getMovies = async () => {
-    const result = await getPopular();
+  const handlePage = (page) => {
+    setCurrentPage(page);
+    getMovies(page);
+  };
+  const getMovies = async (page) => {
+    const result = await getPopular(page);
     setMovies(result);
     setIsLoading(false); // 데이터 가져오기 완료 후 로딩 상태 변경
   };
 
   useEffect(() => {
-    getMovies();
-  }, []); //배열이 비어있으니까 한번만 실행
+    getMovies(currentPage);
+  }, [currentPage]); //배열이 비어있으니까 한번만 실행
   const goToDetailPage = (id) => {
     navigate(`/movies/${id}`);
   };
@@ -51,6 +68,11 @@ export default function Popular() {
           </Container>
         </div>
       )}
+      <Paginate>
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((page) => (
+          <button onClick={() => handlePage(page)}>{page}</button>
+        ))}
+      </Paginate>
     </div>
   );
 }
